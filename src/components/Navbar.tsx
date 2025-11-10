8448c2f91e49765fd941c312f5908a69acfb9e87
coder <donghaattgdtx@gmail.com>
2025-11-10 06:54:16 +0000

    dong bo lan 5

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,10 +1,15 @@
+// src/components/Navbar.tsx
 import React from "react";
 import { Link } from "react-router-dom";
-import "./Navbar.css"; // nhớ tạo file CSS nếu bạn chưa có
+import { useCart } from "../contexts/CartContext";
+import "./Navbar.css";
 
 export const Navbar: React.FC = () => {
+  const { items } = useCart(); // lấy items từ CartContext
+  const cartCount = items.reduce((sum, item) => sum + item.qty, 0);
+
   return (
-    <nav className="Navbar">
+    <nav className="navbar">
       {/* Logo + tên trang */}
       <div className="logo-container">
         <img
@@ -15,11 +20,8 @@ export const Navbar: React.FC = () => {
         <span className="logo-text">Đạo quán Hoyoverse</span>
       </div>
 
-      {/* Menu điều hướng */}
+      {/* Menu điều hướng sang phải */}
       <ul className="menu">
-        <li>
-          <Link to="/">Trang chủ</Link>
-        </li>
         <li>
           <Link to="/characters">Nhân vật</Link>
         </li>
@@ -34,17 +36,10 @@ export const Navbar: React.FC = () => {
             Login
           </Link>
         </li>
-
-        {/* 🔗 Thêm liên kết ngoài – Trang chủ chính thức Genshin Impact */}
         <li>
-          <a
-            href="https://genshin.hoyoverse.com/"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="official-link"
-          >
-            Trang chủ Genshin Impact 🌐
-          </a>
+          <Link to="/cart" className="cart-link">
+            Giỏ hàng ({cartCount})
+          </Link>
         </li>
       </ul>
     </nav>
